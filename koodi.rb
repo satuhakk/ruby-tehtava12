39adef3f80fab64b3e800beafc62f1d7dc7c4b8b
Satu Häkkinen <s.j.hakkinen@gmail.com>
2016-01-21 22:09:46 +0200

    Sorttaushommat ok

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,11 @@
-# kirjoita koodi tänne
+def luokittelu(taulukko)
+
+  hash = {  negatiivinen: taulukko.reject{|item| item >= 0}.sort,
+            positiivinen: taulukko.reject{|item| item < 0}.sort,
+            parillinen: taulukko.select{|item| item.to_i.even?}.sort,
+            pariton: taulukko.select{|item| item.to_i.odd?}.sort,
+            summa: taulukko.inject(0){|tulos, x| tulos + x}}
+end
+
+tauluapukkaa = [1, 3, 54, 7, -1, -55, 0, 0, 1]
+luokittelu(tauluapukkaa)
